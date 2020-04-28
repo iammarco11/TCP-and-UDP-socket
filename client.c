1b4fb79979a6f9eca411a8ab7e3426c667a6479b
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-04-28 18:40:57 +0530

    client: Implement UDP for simple chat purpose

---
+++ client.c
@@ -0,0 +1,41 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+
+#define PORT 8080 
+
+// Driver code 
+int main() { 
+	int sockfd; 
+	char buffer[BUFSIZ]; 
+	char *msg[50]; 
+	struct sockaddr_in servaddr; 
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd<0) { 
+		perror("socket creation failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+
+	memset(&servaddr, 0, sizeof(servaddr)); 
+	
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_port = htons(PORT); 
+	servaddr.sin_addr.s_addr = INADDR_ANY; 
+	
+	int n, len;
+    while(1){
+	    fgets(msg,50,stdin);
+	    sendto(sockfd, (const char *)msg, strlen(msg),MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
+        n = recvfrom(sockfd, (char *)buffer, BUFSIZ,MSG_WAITALL, (struct sockaddr *) &servaddr,	&len); 
+    	buffer[n] = '\0'; 
+	    printf("Server : %s\n", buffer); 
+    }
+	close(sockfd); 
+	return 0; 
+} 
