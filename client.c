b4dc3b87bf0767463ce887f1dd2bc574e47d8f60
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-05-10 20:51:43 +0530

    Client: Add the text file transfer support

--- client.c
+++ client.c
@@ -11,9 +11,11 @@
 
 // Driver code 
 int main() { 
-	int sockfd; 
-	char buffer[BUFSIZ]; 
-	char *msg[50]; 
+	int sockfd;
+	size_t file_size;
+	char *msg; 
+	FILE *fp;
+	char file_name[] = "send.txt";
 	struct sockaddr_in servaddr; 
 
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -27,15 +29,33 @@ int main() {
 	servaddr.sin_family = AF_INET; 
 	servaddr.sin_port = htons(PORT); 
 	servaddr.sin_addr.s_addr = INADDR_ANY; 
+    if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) 
+    { 
+        perror("Connect failed"); 
+        exit(EXIT_FAILURE); 
+    } 
 	
 	int n, len;
-    while(1){
-	    fgets(msg,50,stdin);
-	    sendto(sockfd, (const char *)msg, strlen(msg),MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
-        n = recvfrom(sockfd, (char *)buffer, BUFSIZ,MSG_WAITALL, (struct sockaddr *) &servaddr,	&len); 
-    	buffer[n] = '\0'; 
-	    printf("Server : %s\n", buffer); 
-    }
+	fp = fopen(file_name,"r");
+	if (fp == NULL){
+		perror("File does not exist");
+		exit(EXIT_FAILURE);
+	}
+	printf("File opened successfully\n");
+	fseek(fp,0,SEEK_END);
+	file_size = ftell(fp);
+	fseek(fp,0,SEEK_SET);
+	msg = (char*)calloc(file_size, sizeof(char));
+	printf("%ld\n",sizeof(msg));
+	if(fread(msg, file_size, 1, fp)<=0){
+		perror("Error in copy the file to buffer");
+		exit(EXIT_FAILURE);
+	}
+	printf("File copied into the msg successfully \n");
+	printf("%ld\n",sizeof(msg));
+	printf("%s\n",msg);
+	sendto(sockfd, (const char *)msg, strlen(msg),MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+	fclose(fp);
 	close(sockfd); 
 	return 0; 
 } 
